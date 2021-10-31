25b84f12b3b74d0ae5d878b2d264b2140f439547
Magnus Brantheim <m.brantheim@gmail.com>
2021-10-31 10:55:24 +0100

    Set up deploy to Ropsten

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -6,9 +6,9 @@ import "hardhat/console.sol";
 contract Greeter {
     string private greeting;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    constructor() {
+        console.log("Deploying a Greeter with greeting: Ahoy");
+        greeting = "Ahoy";
     }
 
     function greet() public view returns (string memory) {
