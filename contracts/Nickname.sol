93a83e740eaee8b78ec0e94a7a315243aa16b768
Magnus Brantheim <m.brantheim@gmail.com>
2021-11-01 15:32:01 +0100

    Added setNickname function

--- contracts/Nickname.sol
+++ contracts/Nickname.sol
@@ -10,6 +10,9 @@ contract Nickname {
     
     constructor(address _contractAddress) {
         cte = CaptureTheEther(_contractAddress);
+    }
+
+    function setNickname() public {
         cte.setNickname(0x4f6c6420426f6200000000000000000000000000000000000000000000000000);
     }
 }
